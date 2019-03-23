1ce2d40bdb4bdd4b04beec573ec5b21c4e6222d6
Fernando <fer182_av@hotmail.com>
2019-03-23 18:38:57 -0400

    initial setup

---
+++ src/main/java/edu/umss/dip/svservice/SvServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.umss.dip.svservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SvServiceApplication.class, args);
+    }
+
+}
